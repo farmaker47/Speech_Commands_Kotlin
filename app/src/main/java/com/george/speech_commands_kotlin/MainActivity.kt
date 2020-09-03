5d089e0cec997062ebb5c28acff9b19f595a761a
farmaker47 <farmaker47@gmail.com>
2020-09-03 11:04:00 +0300

    Initial commit

---
+++ app/src/main/java/com/george/speech_commands_kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.george.speech_commands_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
